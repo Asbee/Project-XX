e0e6a44e32e95078235bc9d111659cab353f295b
Asbee <Santtu_boman@hotmail.com>
2016-03-29 09:12:50 +0300

    okokok

---
+++ Game/Building.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Building
+    {
+        public string BName { get; set; }
+        
+        public void BWork()
+        {
+
+        }
+        public void BBuy()
+        {
+            
+        }
+    }
+}
---
+++ Game/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Player
+    {
+        public string PName { get; set; }
+        public string PJob { get; set; }
+        public bool PFood { get; set; }
+        public int PTime { get; set; }
+        public string PItems { get; set; }
+
+        public double PMoney
+        {
+            get
+            {
+                return PMoney;
+            }
+            set
+            {
+                PMoney = 200;
+            }
+        }
+        public int PScore {
+            get
+            {
+                return PScore;
+            }
+            set
+            {
+                PScore = 0;
+            }
+        }
+        public int PHappiness {
+            get
+            {
+                return PHappiness;
+            }
+            set
+            {
+                PHappiness = 0;
+            }
+         }
+
+
+
+
+
+    }
+}
